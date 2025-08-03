aad7c8f7c51604df5a0f8575f6cfb19004c1d2c9
Aristo Apps <aristoappsdev@gmail.com>
2025-08-03 16:20:31 +0530

    Another Change

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/website/', 
   plugins: [react()],
-  server: {
-    port: 5173,
-    host: '0.0.0.0',
-    },
+  // server: {
+  //   port: 5173,
+  //   host: '0.0.0.0',
+  //   },
 })
